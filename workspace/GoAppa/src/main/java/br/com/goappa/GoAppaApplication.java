263f9c867cda6de8d7cbd8d230205f17b76bb2d0
Thyago Monteiro <tmacademico@gmail.com>
2021-03-01 23:36:11 -0300

    Ajuste em CategoriaResourse/Criação de CategoriaService e implementação do domínio Produto

--- workspace/GoAppa/src/main/java/br/com/goappa/GoAppaApplication.java
+++ workspace/GoAppa/src/main/java/br/com/goappa/GoAppaApplication.java
@@ -1,13 +1,35 @@
 package br.com.goappa;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class GoAppaApplication {
+import br.com.goappa.domains.Categoria;
+import br.com.goappa.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class GoAppaApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GoAppaApplication.class, args);
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria (null, "Pizza");
+		Categoria cat2 = new Categoria (null, "Bebida");
+		Categoria cat3 = new Categoria (null, "Sushi");
+		Categoria cat4 = new Categoria (null, "Bebida");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4));
+		
 	}
 
 }
--- workspace/GoAppa/src/main/java/br/com/goappa/resources/CategoriaResourse.java
+++ workspace/GoAppa/src/main/java/br/com/goappa/resources/CategoriaResourse.java
@@ -1,25 +1,24 @@
 package br.com.goappa.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.goappa.domains.Categoria;
+import br.com.goappa.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResourse {
+	@Autowired
+	private CategoriaService service;
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		List<Categoria> lista = new ArrayList<>();
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		lista.addAll(Arrays.asList(cat1, cat2));
-		return lista;
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
