6f36bf73bdda6d902110ba5db05fc385b75cef45
Thyago Monteiro <tmacademico@gmail.com>
2021-03-01 22:25:41 -0300

    Criação de domínio (Categoria) e configuração de banco de dados para teste (H2)

--- workspace/GoAppa/src/main/java/br/com/goappa/resources/CategoriaResourse.java
+++ workspace/GoAppa/src/main/java/br/com/goappa/resources/CategoriaResourse.java
@@ -1,15 +1,25 @@
 package br.com.goappa.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.goappa.domains.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResourse {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "<h1>Isto é um teste</h1>";
+	public List<Categoria> listar() {
+		List<Categoria> lista = new ArrayList<>();
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		lista.addAll(Arrays.asList(cat1, cat2));
+		return lista;
 	}
 }
