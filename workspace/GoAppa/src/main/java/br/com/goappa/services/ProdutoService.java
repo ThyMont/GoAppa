7b10863afffb0ffe353a58455418b66dda002d3e
Thyago Monteiro <tmacademico@gmail.com>
2021-03-01 23:51:39 -0300

    Criação de Produto Repository e Service

---
+++ workspace/GoAppa/src/main/java/br/com/goappa/services/ProdutoService.java
@@ -0,0 +1,23 @@
+package br.com.goappa.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.goappa.domains.Produto;
+import br.com.goappa.repositories.ProdutoRepository;
+import br.com.goappa.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+	@Autowired
+	private ProdutoRepository repo;
+
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+
+}
